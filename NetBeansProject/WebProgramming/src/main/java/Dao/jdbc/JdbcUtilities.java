894b55b2fe227f8412649ffa20ac9d3b35a6c702
AlviseSembenico <alvise.sembenico@studenti.unitn.it>
2017-09-05 17:59:45 +0200

    deleteDao

--- NetBeansProject/WebProgramming/src/main/java/Dao/jdbc/JdbcUtilities.java
+++ NetBeansProject/WebProgramming/src/main/java/Dao/jdbc/JdbcUtilities.java
@@ -236,5 +236,82 @@ public class JdbcUtilities {
     }
     
     
-    //MUST IMPLEMENT DELETEDAO
+    public int deletDao(Object o, HashMap<String, String> map, String tableName) throws SQLException {
+        if (!checkConnection()) {
+            return 0;
+        }
+        if(map==null)
+            map=new <String, String>HashMap();
+        String query = new String("delete from " + tableName+" where");
+        Class<?> c = o.getClass();
+        int id=0;
+        try {
+            for (Method m : c.getDeclaredMethods()) {
+                if (m.getName().contains("get")) {
+                    String name = m.getName().substring(3);
+                    char[] ca = name.toCharArray();
+                    name = String.valueOf(ca[0]).toLowerCase() + name.substring(1);
+                    if(name.equals("id"))
+                        id=(int) m.invoke(o, null);
+                    else if (m.getReturnType().equals(int.class)) {
+                        if ((int) m.invoke(o, null) >= 0) {
+                            if (map.containsKey(name)) {
+                                query += map.get(name) ;
+                            } else {
+                                query += name ;
+                            }
+                            query+=" = "+m.invoke(o, null) + "and";
+                        }
+                    }
+                    else if ( m.getReturnType().equals(double.class)) {
+                        if ((double) m.invoke(o, null) >= 0) {
+                            if (map.containsKey(name)) {
+                                query += map.get(name) ;
+                            } else {
+                                query += name ;
+                            }
+                            query+=" = "+m.invoke(o, null) + "and";
+                        }
+                    }
+                    else if (m.getReturnType().equals(String.class) || m.getReturnType().equals(Date.class)) {
+                        if ((Object) m.invoke(o, null) != null) {
+                            if (map.containsKey(name)) {
+                                query += map.get(name);
+                            } else {
+                                query += name ;
+                            }
+                            query+=" = '"+m.invoke(o, null) + "'and";
+                        }
+                    }
+                    else{
+                        Object obj = m.invoke(o, null);
+                        if (obj != null) {
+                            if (obj instanceof IdOwner) {
+                                IdOwner idOwner = (IdOwner) obj;
+                                if (map.containsKey(name)) {
+                                    query += map.get(name);
+                                } else {
+                                    query += name ;
+                                }
+                                query+="="+idOwner.getId()+",";
+                            } 
+                        }
+                    }
+                    
+
+                }
+            }
+        } catch (Exception ex) {
+            return 0;
+        }
+        query = query.substring(0, query.length() - 1);
+        
+        if(query.length()<20)
+            return 2;
+        if(id>0)
+            query+=" where id ="+id;
+        PreparedStatement stmt=connection.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+
+        return stmt.executeUpdate();
+    }
 }
