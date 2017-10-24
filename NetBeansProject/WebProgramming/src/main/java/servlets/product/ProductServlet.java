978409cdeeb41ed47de70d903e1af924690e7af2
Luca Zappini <luca.zappini-1@studenti.unitn.it>
2017-10-24 14:49:23 +0200

    sistemazione1.2

--- NetBeansProject/WebProgramming/src/main/java/servlets/product/ProductServlet.java
+++ NetBeansProject/WebProgramming/src/main/java/servlets/product/ProductServlet.java
@@ -30,92 +30,92 @@ import javax.servlet.http.HttpSession;
  */
 public class ProductServlet extends HttpServlet {
 
-        private ProductDao productDao;
-        private UserDao userDao;
-        private CartDao cartDao;
-        private PictureDao pictureDao;
+    private ProductDao productDao;
+    private UserDao userDao;
+    private CartDao cartDao;
+    private PictureDao pictureDao;
 
-        @Override
-        public void init() throws ServletException {
-            productDao = (ProductDao) super.getServletContext().getAttribute("productDao");
-            if (productDao == null) {
-                throw new ServletException("Impossible to get dao factory for user storage system");
-            }
-            userDao = (UserDao) super.getServletContext().getAttribute("userDao");
-            if (userDao == null) {
-                throw new ServletException("Impossible to get dao factory for user storage system");
-            }
-            cartDao = (CartDao) super.getServletContext().getAttribute("cartDao");
-            if (cartDao == null) {
-                throw new ServletException("Impossible to get dao factory for user storage system");
-            }
-            pictureDao = (PictureDao) super.getServletContext().getAttribute("pictureDao");
-            if (pictureDao == null) {
-                throw new ServletException("Impossible to get dao factory for user storage system");
-            }
+    @Override
+    public void init() throws ServletException {
+        productDao = (ProductDao) super.getServletContext().getAttribute("productDao");
+        if (productDao == null) {
+            throw new ServletException("Impossible to get dao factory for user storage system");
         }
-
-        /**
-         * Handles the HTTP <code>GET</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-            try {
-                int id = Integer.parseInt(request.getParameter("id"));
-                Product product = productDao.getProductById(id);
-                LinkedList<Picture> pictures = pictureDao.getPictureByProduct(product);
-                request.setAttribute("product", product);
-                request.setAttribute("pictures", pictures);
-                RequestDispatcher reqDes = request.getRequestDispatcher("/product.jsp");
-                reqDes.forward(request, response);
-            } catch (Exception ex) {
-                Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        userDao = (UserDao) super.getServletContext().getAttribute("userDao");
+        if (userDao == null) {
+            throw new ServletException("Impossible to get dao factory for user storage system");
         }
-
-        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-        /**
-         * Handles the HTTP <code>POST</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doPost(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-            try {
-                int pid = Integer.parseInt(request.getParameter("pid"));
-                HttpSession session = request.getSession(false);
-                Product product = productDao.getProductById(pid);
-                if (product != null) {
-                    Cart cart = (Cart) session.getAttribute("cart");
-                    if (cart == null) {
-                        cart = new Cart((User) request.getSession(false).getAttribute("user"));
-                    }
-                    cart.AddProducts(product);
-                    session.setAttribute("cart", cart);
-                }
-                response.sendRedirect(response.encodeRedirectURL("cart"));
-            } catch (Exception ex) {
-                Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        cartDao = (CartDao) super.getServletContext().getAttribute("cartDao");
+        if (cartDao == null) {
+            throw new ServletException("Impossible to get dao factory for user storage system");
         }
-
-        /**
-         * Returns a short description of the servlet.
-         *
-         * @return a String containing servlet description
-         */
-        @Override
-        public String getServletInfo() {
-            return "Short description";
-        }// </editor-fold>
+        pictureDao = (PictureDao) super.getServletContext().getAttribute("pictureDao");
+        if (pictureDao == null) {
+            throw new ServletException("Impossible to get dao factory for user storage system");
+        }
     }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            int id = Integer.parseInt(request.getParameter("id"));
+            Product product = productDao.getProductById(id);
+            LinkedList<Picture> pictures = pictureDao.getPictureByProduct(product);
+            request.setAttribute("product", product);
+            request.setAttribute("pictures", pictures);
+            RequestDispatcher reqDes = request.getRequestDispatcher("/product.jsp");
+            reqDes.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            int pid = Integer.parseInt(request.getParameter("pid"));
+            HttpSession session = request.getSession(false);
+            Product product = productDao.getProductById(pid);
+            if (product != null) {
+                Cart cart = (Cart) session.getAttribute("cart");
+                if (cart == null) {
+                    cart = new Cart((User) request.getSession(false).getAttribute("user"));
+                }
+                cart.AddProducts(product);
+                session.setAttribute("cart", cart);
+            }
+            response.sendRedirect(response.encodeRedirectURL("cart"));
+        } catch (Exception ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
