97beed6a6ac5374d6d2aabac2c8132a3da8b1c21
ZappO <luca.zappini-1@studenti.unitn.it>
2017-11-20 12:09:35 +0100

    modifiche

--- NetBeansProject/WebProgramming/src/main/java/servlets/recover/recover.java
+++ NetBeansProject/WebProgramming/src/main/java/servlets/recover/recover.java
@@ -13,8 +13,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
@@ -39,6 +41,9 @@ public class recover extends HttpServlet {
             throw new ServletException("Impossible to get dao factory for user storage system");
         }
     }
+    private String SMTP_HOST = "smtp.gmail.com";
+    private String SMTP_USER = "noreply.webproject@gmail.com";
+    private String SMTP_PASSWD = "Sistemi2";
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -53,51 +58,56 @@ public class recover extends HttpServlet {
             throws ServletException, IOException {
         User user = null;
         try {
-            user = userDao.getUserByEmail(request.getParameter("email"));
+            user = userDao.getUserByEmail((String) request.getParameter("email"));
         } catch (Exception ex) {
-            Logger.getLogger(recover.class.getName()).log(Level.SEVERE, null, ex);
+            user = null;
         }
-
-        if (!user.getEmail().isEmpty()) {
-            // Recipient's email ID needs to be mentioned.
-            String to = user.getEmail();
-
-            // Sender's email ID needs to be mentioned
-            String from = "noreplay@o2.ie";
-
-            // Assuming you are sending email from localhost
-            String host = "smtp.o2.ie";
-
-            // Get system properties
-            Properties properties = System.getProperties();
-
-            // Setup mail server
-            properties.setProperty("mail.smtp.host", host);
-
-            // Get the default Session object.
-            Session session = Session.getDefaultInstance(properties);
+        if (user != null) {
 
             try {
-                // Create a default MimeMessage object.
-                MimeMessage message = new MimeMessage(session);
-                // Set From: header field of the header.
-                message.setFrom(new InternetAddress(from));
-                // Set To: header field of the header.
-                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-                // Set Subject: header field
-                message.setSubject("Recover password");
-                // Now set the actual message
-                message.setText("this is your password: " + user.getPassword());
-                // Send message
+                Properties props = new Properties();
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.starttls.enable", "true");
+                props.put("mail.smtp.host", SMTP_HOST);
+                props.put("mail.smtp.port", "587");
+
+                Session session = Session.getInstance(props,
+                        new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(SMTP_USER, SMTP_PASSWD);
+                    }
+                });
+
+                
+                String link = request.getRequestURL().toString();
+                link = link.substring(0, 36);
+
+                StringBuilder bodyText = new StringBuilder();
+                bodyText.append("<div>")
+                        .append("  Dear User<br/><br/>")
+                        .append("  We got your reset password request, Find below link to reset password <br/>")
+                        .append("  Please click <a href=\"" + link + "\">here</a> or open below link in browser<br/>")
+                        .append("  <a href=\"" + link + "\">" + link + "</a>")
+                        .append("  <br/><br/>")
+                        .append("  Thanks,<br/>")
+                        .append("  SodhanaLibrary Team")
+                        .append("</div>");
+                Message message = new MimeMessage(session);
+
+                message.setFrom(new InternetAddress(SMTP_USER));
+
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse(user.getEmail()));
+                message.setSubject("Reset Password");
+                message.setContent(bodyText.toString(), "text/html; charset=utf-8");
                 Transport.send(message);
-                response.sendRedirect(response.encodeRedirectURL(getServletContext().getContextPath() + "login"));                
-                        
-            } catch (MessagingException mex) {
-                mex.printStackTrace();
+            } catch (AddressException ex) {
+                Logger.getLogger(recover.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (MessagingException ex) {
+                Logger.getLogger(recover.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-
     /**
      * Returns a short description of the servlet.
      *
